16106990e40c47f2d44367fb6d0cdcf2f80d9242
Ahmad Al-Deeb <ahmeddeeb22222@gmail.com>
2024-03-01 07:41:26 +0200

    Register a new setting section

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,38 @@
 import joplin from 'api';
-import { ContentScriptType } from 'api/types';
+import { ContentScriptType, SettingItemType } from 'api/types';
 
+
+
+////////// Register a setting //////////
+const highlightLineSettingId = 'highlight-active-line';
+const registerSettings = async () => {
+	const sectionName = 'example-cm6-plugin';
+	
+	// The call to joplin.settings.registerSection creates a new section in Joplin's settings.
+	// This is where we'll put new settings.
+	await joplin.settings.registerSection(sectionName, {
+			label: 'CodeMirror 6 demo plugin',
+			description: 'Settings for the CodeMirror 6 example plugin.',
+			iconName: 'fas fa-edit',
+	});
+
+	//  register a setting with highlightLineSettingId as its ID
+	await joplin.settings.registerSettings({
+		[highlightLineSettingId]: {
+				section: sectionName,
+				value: true, // Default value
+				public: true, // Show in the settings screen
+				type: SettingItemType.Bool,
+				label: 'Highlight active line',
+		},
+	});
+};
+
+////////// Register Plugin //////////
 joplin.plugins.register({
 	onStart: async function() {
+		await registerSettings();
+		
 		const contentScriptId = 'some-content-script-id';
 		joplin.contentScripts.register(
 				ContentScriptType.CodeMirrorPlugin,
