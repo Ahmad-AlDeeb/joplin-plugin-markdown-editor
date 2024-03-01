034b9907db0229da5caaad5f57470699c29275ee
Ahmad Al-Deeb <ahmeddeeb22222@gmail.com>
2024-03-01 09:15:40 +0200

    Get the setting from content script

--- src/contentScript.ts
+++ src/contentScript.ts
@@ -1,12 +1,25 @@
 // 1. Import a CodeMirror extension
-import joplin from "api";
-const { lineNumbers } = joplin.require('@codemirror/view');
+// linenumber is built-in CodeMirror extension to add line numbers in editor
+// highlightActiveLine is another built-in CodeMirror extension. It adds the cm-activeLine class to all lines that have a cursor on them.
+import { lineNumbers, highlightActiveLine } from '@codemirror/view';
 
-export default (_context: { contentScriptId: string, postMessage: any }) => {
+export default (context: { contentScriptId: string, postMessage: any }) => {
     return {
-        plugin: (codeMirrorWrapper: any) => {
+        
+        // An `async` was also added so that we can `await` the result of `context.postMessage`:
+        plugin: async (codeMirrorWrapper: any) => {
             // 2. Adds the built-in CodeMirror 6 extension to the editor
             codeMirrorWrapper.addExtension(lineNumbers());
+
+            // Get settings from the main script with postMessage:
+            //  We get settings from index.ts with context.postMessage('getSettings')
+            // This calls the onMessage listener that was registered in index.ts file
+            const settings = await context.postMessage('getSettings');
+            if (settings.highlightActiveLine) {
+                codeMirrorWrapper.addExtension(highlightActiveLine());
+            }
         },
+
+        
     };
 };
